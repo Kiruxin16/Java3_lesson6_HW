46bef2325e71dda6722f70394b5c5fea13362deb
Kirill <kiruxin16@gmail.com>
2022-04-07 22:56:13 +0700

    Тестирование методов с массивами

---
+++ src/main/java/ru/hw/SimpleArrWorking.java
@@ -0,0 +1,11 @@
+package ru.hw;
+
+public class SimpleArrWorking implements ArrWorking {
+    public int[] afterFour(int[] arr) {
+        return new int[0];
+    }
+
+    public boolean hasOnlyOneOrFour(int[] arr) {
+        return false;
+    }
+}
